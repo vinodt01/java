031e6cab725be060f9043b2771bce20e1e328336
vinodt01 <58156620+vinodt01@users.noreply.github.com>
2020-01-06 00:43:21 -0500

    Delete properties.java

--- src/com/gvta/email/properties.java
+++
@@ -1,8 +0,0 @@
-package com.gvta.email;
-
-public class properties {
-	public static String FROMEMAIL = "gvt.abhinav1995@gmail.com";
-	public static String TOEMAIL = "gvt.abhinav1995@gmail.com";
-	public static String PASSWORD = "Bond2009@";
-
-}
